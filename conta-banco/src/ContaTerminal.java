c8893d4128cda31d31fed5a2afcd85c9533036b7
Luiz Ricardo de Campos <playsonbr@hotmail.com>
2024-05-07 15:42:01 -0300

    Criada a classe ContaTerminal

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //Criano o objeto scanner
+        Scanner scanner = new Scanner(System.in);
+       
+        //Declaração das variaveis.
+         int numero;
+         String agencia;
+         String nomeCliente;
+         double saldo;
+
+         //Texto na tela e recebimento dos dados
+        System.out.println("Digite um numero para a conta: ");
+            numero = scanner.nextInt();
+
+        System.out.println("Digite um numero para a agencia: ");
+            agencia = scanner.next();
+
+        System.out.println("Digite o seu nome: ");
+            nomeCliente = scanner.next();
+
+        System.out.println("Digite o valor do seu primeiro depósito: ");
+             saldo = scanner.nextDouble();
+
+
+     //Saida com os dados        
+    System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "+ agencia +", conta "+ numero +" e seu saldo "+ saldo +" já está disponível para saque");
     }
 }
